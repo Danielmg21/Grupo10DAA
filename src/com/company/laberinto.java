088e2ca0cda6d3ffb6488cb6e97b61542181291d
Danielmg21 <46795242+Danielmg21@users.noreply.github.com>
2019-03-16 12:07:48 +0100

    fallos
    seleccionActividades
    laberinto

--- src/com/company/laberinto.java
+++ src/com/company/laberinto.java
@@ -14,6 +14,52 @@ public class laberinto {
         }
     }
 
+    public static void copiar (char tablero[][],char mejorTablero[][]){
+            int contX=0;
+            for(int i=0;i<mejorTablero.length;i++){
+                for(int j=0;j<mejorTablero.length;j++){
+                    mejorTablero[i][j]=tablero[i][j];
+                    if(mejorTablero[i][j]=='X'){
+                        contX++;
+                    }
+                }
+            }
+            System.out.println("Nueva solucion con "+contX+" pasos");
+    }
+
+    public static boolean esMejor (char tablero[][],char mejorTablero[][]){
+        int contx1=0;
+        for(int i=0;i<tablero.length;i++){
+            for(int j=0;j<tablero.length;j++){
+                if(tablero[i][j]=='X'){
+                    contx1++;
+                }
+            }
+        }
+        int contx2=0;
+        for(int i=0;i<mejorTablero.length;i++){
+            for(int j=0;j<mejorTablero.length;j++){
+                if(mejorTablero[i][j]=='X'){
+                    contx2++;
+                }
+            }
+        }
+
+        return (contx1<contx2);
+
+
+    }
+    public static boolean esFactibleMejor (char tablero[][],int etapax,int etapay, int numpasos,int nuevax,int nuevay,int intento,char mejorTablero[][]){
+        if((nuevax<0)||(nuevax >= tablero.length)||(nuevay<0)||(nuevay >= tablero.length))
+            return false;
+        if((tablero[nuevax][nuevay]=='M')||(tablero[nuevax][nuevay]=='E')||(tablero[nuevax][nuevay]=='X'))
+            return false;
+        if (esMejor (mejorTablero,tablero)){
+            return false;
+        }
+        return true;
+    }
+
     public static boolean esFactible (char tablero[][],int etapax,int etapay,int numPaso,int nuevax,int nuevay){
         if((nuevax<0)||(nuevax>=tablero.length)||(nuevay<0)||(nuevay>=tablero.length))
             return false;
@@ -41,6 +87,41 @@ public class laberinto {
         }
         return exito;
     }
+
+    public static void BTtodas (char tablero[][],int etapax,int etapay,int numPaso){
+        for (int intento=0;intento< mov_rel_x.length;intento++){
+            int nuevax = etapax+mov_rel_x[intento];
+            int nuevay = etapax+mov_rel_y[intento];
+            if (esFactible(tablero,etapax,etapay,numPaso,nuevax,nuevay)){
+                if(tablero[nuevax][nuevay]=='S'){
+                    System.out.println("Solucion encontrada");
+                    imprimir(tablero);
+                }else{
+                    tablero[nuevax][nuevay]='X';
+                    BTtodas(tablero,nuevax,nuevay,numPaso+1);
+                    tablero[nuevax][nuevay]=' ';
+                }
+            }
+        }
+    }
+
+    public static void BTMejor (char tablero[][],int etapax,int etapay,int numPaso,char mejorTablero[][]){
+        for (int intento=0;intento<mov_rel_x.length;intento++){
+            int nuevax = etapax+mov_rel_x[intento];
+            int nuevay = etapax+mov_rel_y[intento];
+            if(esFactibleMejor(tablero,etapax,etapay,numPaso,nuevax,nuevay,intento,mejorTablero)){
+                if(tablero[nuevax][nuevay]=='S'){
+                    if(esMejor(tablero,mejorTablero)){
+                        copiar(tablero,mejorTablero);
+                    }
+                }else{
+                    tablero[nuevax][nuevay]='X';
+                    BTMejor(tablero,nuevax,nuevay,numPaso+1,mejorTablero);
+                    tablero[nuevax][nuevay]=' ';
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         char tablero [][] = new char[9][9];
         for(int i = 0;i<tablero.length;i++){
@@ -56,12 +137,21 @@ public class laberinto {
         tablero[1][3] = 'M';
         tablero[1][4] = 'M';
         tablero[1][5] = 'M';
-
+    /*
         boolean exito = BT (tablero,0,0,1);
         if(exito){
             imprimir (tablero);
         }else{
             System.out.println("No hay solucion");
         }
+        */
+        char mejorTablero [][] = new char [9][9];
+        for (int i=0;i<mejorTablero.length;i++){
+            for(int j=0;j<mejorTablero.length;j++){
+                mejorTablero[i][j]='X';
+            }
+        }
+        BTMejor(tablero,0,0,1,mejorTablero);
+        imprimir(mejorTablero);
     }
 }
