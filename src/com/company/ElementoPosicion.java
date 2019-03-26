94031144daf377f59cf433d5ed226dc6470a3061
Alfonso Pajuelo <46894176+alfonsopajueloaguirre@users.noreply.github.com>
2019-03-27 00:43:46 +0100

    Comenzado voraz elemento en una posicion 5%

---
+++ src/com/company/ElementoPosicion.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ElementoPosicion {
+    public static void main (String []args){
+        Scanner entrada = new Scanner(System.in);
+        int longitudvector = entrada.nextInt();
+        String vectorString = entrada.nextLine();
+
+    }
+}
