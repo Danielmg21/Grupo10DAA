9a3245b1fcf2dd12a9d503fdf220cbb0812f351e
Danielmg21 <46795242+Danielmg21@users.noreply.github.com>
2019-05-09 13:48:22 +0200

    tonto

--- src/BT/saltoCaballo.java
+++ src/BT/saltoCaballo.java
@@ -34,6 +34,21 @@ public class saltoCaballo {
         }return exito;
     }
 
+    public static void BTTodas (int [][]tablero,int etapa,int pos_actualX,int pos_actualY){
+        for (int intento = 0;intento<=7;intento++){
+            int pos_nuevaX = pos_actualX + movRealX[intento];
+            int pos_nuevaY = pos_actualY + movRealY[intento];
+            if(esFactible(tablero,etapa,pos_nuevaX,pos_nuevaY)){
+                tablero[pos_nuevaX][pos_nuevaY] = etapa;
+                if (etapa == 25){
+                    imprimir(tablero);
+                }else{
+                    BT(tablero,etapa+1,pos_nuevaX,pos_nuevaY);
+                }
+                tablero[pos_nuevaX][pos_nuevaY]=0;
+            }
+        }
+    }
 
     public static boolean esFactible (int tablero[][],int etapa,int pos_nuevaX,int pos_nuevaY){
         if ((pos_nuevaX>4)||(pos_nuevaX<0)){
@@ -49,13 +64,17 @@ public class saltoCaballo {
     }
 
     public static void main(String[] args) {
-        int [][]tablero = new int [5][5];
+        /*int [][]tablero = new int [5][5];
         tablero[1][1] = 1;
-        saltoCaballo s = new saltoCaballo();
-        boolean exito = s.BT (tablero,2,1,1);
+        boolean exito = BT (tablero,2,1,1);
         if (exito){
-            s.imprimir (tablero);
+            imprimir (tablero);
         }
+        */
+
+        int [][]tablero2 = new int [5][5];
+        tablero2[1][1]=1;
+        BTTodas(tablero2,2,1,1);
     }
 }
 
